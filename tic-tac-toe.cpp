1d3580fe559a069048294e19ac88afa4f1d56ae0
ltlexi140 <ltlexi1408@proton.me>
2025-01-09 19:21:01 +1100

    print board function

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 
+void PrintBoard(char (&board)[3][3]);
+
 int main(){
 
     char board[3][3] {0};
 
+    board[1][2] = 'X';
+
+    PrintBoard(board);
+
     return 0;
+}
+
+void PrintBoard(char (&board)[3][3]){
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            std::cout << board[i][j];
+        }
+        std::cout << std::endl;
+    }
 }
